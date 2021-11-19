3ce3a4bcfe73d69c268c41ed6f5c44c32dbe9c81
LuboNenov <liubomir.n.2019@feg.bg>
2021-11-19 22:56:31 +0200

    Fixed identation issues

--- ragnarok_server/modules/port_finder.py
+++ ragnarok_server/modules/port_finder.py
@@ -1,23 +1,25 @@
-import argparse
 from threading import Thread, Lock
 import socket
 from queue import Queue
 
+queue = Queue()
+
 class PortFinder:
-    N_THREADS = 100
-    queue = Queue()
-    protocol = 'tcp'
     def pscan(self, port):
         """ Port scanner made to discover services and addresses (optimised with multithreading) """
         try:
             starter = socket.socket()
             starter.connect((target_ip, port))
+
         except:
             with Lock():
                 skippable = True
+
         else:
             with Lock():
-                print("Port: %s => %s" %(port, socket.getservbyport(port, pro)))
+                protocol = 'tcp'
+                print("Port: %s => %s" %(port, socket.getservbyport(port, protocol)))
+
         finally:
             starter.close()
 
@@ -26,16 +28,20 @@ class PortFinder:
         global queue
         while True:
             num = queue.get()
-            pscan(num)
+            PortFinder().pscan(num)
             queue.task_done()
 
     def main(self, target_ip, ports):
         """ Definiton of the ip to be scanned using a priority queue """
         global queue
+        N_THREADS = 100
+
         for thread in range (N_THREADS):
-            thread = Thread(target = scan_thread)
+            thread = Thread(target = PortFinder().scan_thread)
             thread.daemon = True
             thread.start()
+
         for num in ports:
             queue.put(num)
-            queue.join()
+
+        queue.join()
