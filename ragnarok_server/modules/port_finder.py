0b1ae3d01aaae2eee5e074ba2a28547abb8ccfba
LuboNenov <liubomir.n.2019@feg.bg>
2022-02-08 21:09:00 +0200

    Fixed minor issues

--- ragnarok_server/modules/port_finder.py
+++ ragnarok_server/modules/port_finder.py
@@ -2,23 +2,23 @@ from threading import Thread, Lock
 import socket
 from queue import Queue
 
-queue = Queue()
-
 class PortFinder:
+    global queue
+    queue = Queue()
+    global output
+    output = []
     def pscan(self, port):
         """ Port scanner made to discover services and addresses (optimised with multithreading) """
         try:
             starter = socket.socket()
             starter.connect((target_ip, port))
-
         except:
             with Lock():
                 skippable = True
-
         else:
             with Lock():
                 protocol = 'tcp'
-                print("Port: %s => %s" %(port, socket.getservbyport(port, protocol)))
+                output.append('Port: %s => %s' %(port, socket.getservbyport(port, protocol)))
 
         finally:
             starter.close()
@@ -35,13 +35,11 @@ class PortFinder:
         """ Definiton of the ip to be scanned using a priority queue """
         global queue
         N_THREADS = 100
-
         for thread in range (N_THREADS):
             thread = Thread(target = PortFinder().scan_thread)
             thread.daemon = True
             thread.start()
-
         for num in ports:
             queue.put(num)
-
         queue.join()
+        return output
